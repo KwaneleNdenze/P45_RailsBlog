07207458e46c2fbe2a919970ef724ac88cbe2d1f
Kwanele <kndenze@gmail.com>
2022-03-16 13:30:00 +0200

    RailsBlog upload

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-    #redirect_to article_path(@article)
+    
     flash[:notice] = "Article was created successfully"
     redirect_to @article
     else
